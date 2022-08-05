ae7e5075ea9fbc50bed4bd28a3c79280196fd144
Hahyeon Lee <hahlee@c3r3s4.42seoul.kr>
2022-08-05 14:02:26 +0900

    retry

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hahlee <hahlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 12:25:27 by hahlee            #+#    #+#             */
-/*   Updated: 2022/08/02 20:49:40 by hahlee           ###   ########.fr       */
+/*   Updated: 2022/08/04 16:34:45 by hahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	byte;
 
 	byte = count * size;
-	if (byte < count || byte < size)
+	if (byte && (byte < count || byte < size))
 		return (NULL);
 	ptr = malloc(byte);
 	if (ptr == NULL)
